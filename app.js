bd8a27b95e7d83c6e98d6dbcbd6e0530cac4d3bf
Dennis Williams <Dennis Williams>
2017-02-20 04:53:52 -0600

    only use logger in development tier

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const models = require('./models');
 const app = express();
 
 // Load middleware that will execute on each request
-app.use(logger('dev'));
+if (tier === 'development') {
+  app.use(logger('dev'));
+}
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: false, limit: '5mb' }));
 app.use(cookieParser());
