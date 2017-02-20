947b968c04bfd09bba0fe6fe1f9bb77402e5d469
Dennis Williams <Dennis Williams>
2017-02-20 05:53:43 -0600

    improved organization of integration tests

--- test/integration/authenticate.js
+++ test/integration/authenticate.js
@@ -4,32 +4,34 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('routes/authentication', function () {
-  it('should return status 200 and a valid JSON web token on /authenticate POST', function (done) {
-    chai
-      .request(global.server)
-      .post('/authenticate')
-      .send({username: 'TestUser', password: 'TestPassword'})
-      .end(function (error, response, body) {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.have.property('status', 'success');
-        response.body.should.have.property('data');
-        response.body.data.should.have.property('accessToken');
-        done();
-      });
-  });
-  
-  it('should return status 401 and when bad credentials are sent on /authenticate POST', function (done) {
-    chai
-      .request(global.server)
-      .post('/authenticate')
-      .send({username: 'TestUser', password: 'BadPassword'})
-      .end(function (error, response, body) {
-        response.should.have.status(401);
-        response.should.be.json;
-        response.body.should.have.property('status', 'fail');
-        done();
-      });
+describe('routes/authenticate', function () {
+  describe('post', function () {
+    it('should return status 401 and when bad credentials are sent on /authenticate POST', function (done) {
+      chai
+        .request(global.server)
+        .post('/authenticate')
+        .send({username: 'TestUser', password: 'BadPassword'})
+        .end(function (error, response, body) {
+          response.should.have.status(401);
+          response.should.be.json;
+          response.body.should.have.property('status', 'fail');
+          done();
+        });
+    });
+    
+    it('should return status 200 and a valid JSON web token on /authenticate POST', function (done) {
+      chai
+        .request(global.server)
+        .post('/authenticate')
+        .send({username: 'TestUser', password: 'TestPassword'})
+        .end(function (error, response, body) {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.have.property('status', 'success');
+          response.body.should.have.property('data');
+          response.body.data.should.have.property('accessToken');
+          done();
+        });
+    });
   });
 });
--- test/integration/users.js
+++ test/integration/users.js
@@ -5,128 +5,174 @@ const should = chai.should();
 chai.use(chaiHttp);
 
 let self = this;
+self.dummyUser = {
+  username: 'DummyUser',
+  password: 'DummyPassword',
+  roles: [{ id: 1, name: 'admin' }]
+};
 
 describe('routes/users', function () {
-  before(function () {
-    self.dummyUser = {
-      username: 'DummyUser',
-      password: 'DummyPassword',
-      roles: [{ id: 1, name: 'admin' }]
-    };
+  describe('get', function () {
+    it('should return status 401 unauthorized due to lack of auth token on /users GET', function (done) {
+      chai
+        .request(global.server)
+        .get('/users')
+        .end(function (error, response, body) {
+          response.should.have.status(401);
+          response.should.be.json;
+          response.body.should.have.property('status', 'fail');
+          response.body.should.have.property('data').that.is.an('object');
+          response.body.data.should.have.property('message');
+          done();
+        });
+    });
+    
+    it('should return status 401 unauthorized due to lack of auth token on /users/:userId GET', function (done) {
+      chai
+        .request(global.server)
+        .get(`/users/${global.testUser.id}`)
+        .end(function (error, response, body) {
+          response.should.have.status(401);
+          response.should.be.json;
+          response.body.should.have.property('status', 'fail');
+          response.body.should.have.property('data').that.is.an('object');
+          response.body.data.should.have.property('message');
+          done();
+        });
+    });
+    
+    it('should return status 200 and a valid JSON object containing an array of users on /users GET', function (done) {
+      chai
+        .request(global.server)
+        .get('/users')
+        .set('accessToken', global.authToken)
+        .end(function (error, response, body) {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.have.property('status', 'success');
+          response.body.should.have.property('data').that.is.an('array').that.is.not.empty;
+          response.body.data[0].should.have.property('username');
+          response.body.data[0].should.have.property('roles').that.is.an('array');
+          done();
+        });
+    });
+    
+    it('should return status 200 and a valid JSON object containing an array of users on /users?searchParams GET', function (done) {
+      chai
+        .request(global.server)
+        .get(`/users?username=${global.testUser.username}`)
+        .set('accessToken', global.authToken)
+        .end(function (error, response, body) {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.have.property('status', 'success');
+          response.body.should.have.property('data').that.is.an('array').that.is.not.empty;
+          response.body.data[0].should.have.property('username');
+          response.body.data[0].should.have.property('roles').that.is.an('array').that.is.not.empty;
+          response.body.data[0].roles[0].should.have.property('name', 'admin');
+          done();
+        });
+    });
+    
+    it('should return status 200 and a valid JSON object containing a single user on /users/:userId GET', function (done) {
+      chai
+        .request(global.server)
+        .get(`/users/${global.testUser.id}`)
+        .set('accessToken', global.authToken)
+        .end(function (error, response, body) {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.have.property('status', 'success');
+          response.body.should.have.property('data').that.is.an('object');
+          response.body.data.should.have.property('username');
+          done();
+        });
+    });
   });
   
-  it('should return status 401 unauthorized due to lack of auth token on /users GET', function (done) {
-    chai
-      .request(global.server)
-      .get('/users')
-      .end(function (error, response, body) {
-        response.should.have.status(401);
-        response.should.be.json;
-        response.body.should.have.property('status', 'fail');
-        response.body.should.have.property('data').that.is.an('object');
-        response.body.data.should.have.property('message');
-        done();
-      });
+  describe('put', function () {    
+    it('should return status 200 and a valid JSON object containing an inserted user on /users PUT', function (done) {
+      chai
+        .request(global.server)
+        .put('/users')
+        .set('accessToken', global.authToken)
+        .send(self.dummyUser)
+        .end(function (error, response, body) {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.have.property('status', 'success');
+          response.body.should.have.property('data').that.is.an('object');
+          response.body.data.should.have.property('username');
+          response.body.data.should.have.property('roles').that.is.an('array').that.is.not.empty;
+          response.body.data.roles[0].should.have.property('name', 'admin');
+          self.dummyUser = response.body.data;
+          done();
+        });
+    });
   });
   
-  it('should return status 200 and a valid JSON object containing an array of users on /users GET', function (done) {
-    chai
-      .request(global.server)
-      .get('/users')
-      .set('accessToken', global.authToken)
-      .end(function (error, response, body) {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.have.property('status', 'success');
-        response.body.should.have.property('data').that.is.an('array').that.is.not.empty;
-        response.body.data[0].should.have.property('username');
-        response.body.data[0].should.have.property('roles').that.is.an('array').that.is.not.empty;
-        response.body.data[0].roles[0].should.have.property('name', 'admin');
-        done();
-      });
+  describe('post', function () {
+    it('should return status 401 unauthorized due to lack of auth token on /users POST', function (done) {
+      chai
+        .request(global.server)
+        .post(`/users/${self.dummyUser.id}`)
+        .end(function (error, response, body) {
+          response.should.have.status(401);
+          response.should.be.json;
+          response.body.should.have.property('status', 'fail');
+          response.body.should.have.property('data').that.is.an('object');
+          response.body.data.should.have.property('message');
+          done();
+        });
+    });
+    
+    it('should return status 200 and a valid JSON object containing an updated user on /users POST', function (done) {
+      chai
+        .request(global.server)
+        .post(`/users/${self.dummyUser.id}`)
+        .set('accessToken', global.authToken)
+        .send(self.dummyUser)
+        .end(function (error, response, body) {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.have.property('status', 'success');
+          response.body.should.have.property('data').that.is.an('object');
+          response.body.data.should.have.property('username');
+          response.body.data.should.have.property('roles').that.is.an('array').that.is.not.empty;
+          response.body.data.roles[0].should.have.property('name', 'admin');
+          done();
+        });
+    });
   });
   
-  it('should return status 200 and a valid JSON object containing an array of users on /users?searchParams GET', function (done) {
-    chai
-      .request(global.server)
-      .get(`/users?username=${global.testUser.username}`)
-      .set('accessToken', global.authToken)
-      .end(function (error, response, body) {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.have.property('status', 'success');
-        response.body.should.have.property('data').that.is.an('array').that.is.not.empty;
-        response.body.data[0].should.have.property('username');
-        response.body.data[0].should.have.property('roles').that.is.an('array').that.is.not.empty;
-        response.body.data[0].roles[0].should.have.property('name', 'admin');
-        done();
-      });
-  });
-  
-  it('should return status 200 and a valid JSON object containing a single user on /users/:userId GET', function (done) {
-    chai
-      .request(global.server)
-      .get(`/users/${global.testUser.id}`)
-      .set('accessToken', global.authToken)
-      .end(function (error, response, body) {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.have.property('status', 'success');
-        response.body.should.have.property('data').that.is.an('object');
-        response.body.data.should.have.property('username');
-        done();
-      });
-  });
-  
-  it('should return status 200 and a valid JSON object containing an inserted user on /users PUT', function (done) {
-    chai
-      .request(global.server)
-      .put('/users')
-      .set('accessToken', global.authToken)
-      .send(self.dummyUser)
-      .end(function (error, response, body) {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.have.property('status', 'success');
-        response.body.should.have.property('data').that.is.an('object');
-        response.body.data.should.have.property('username');
-        response.body.data.should.have.property('roles').that.is.an('array').that.is.not.empty;
-        response.body.data.roles[0].should.have.property('name', 'admin');
-        self.dummyUser = response.body.data;
-        done();
-      });
-  });
-  
-  it('should return status 200 and a valid JSON object containing an updated user on /users POST', function (done) {
-    chai
-      .request(global.server)
-      .post(`/users/${self.dummyUser.id}`)
-      .set('accessToken', global.authToken)
-      .send(self.dummyUser)
-      .end(function (error, response, body) {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.have.property('status', 'success');
-        response.body.should.have.property('data').that.is.an('object');
-        response.body.data.should.have.property('username');
-        response.body.data.should.have.property('roles').that.is.an('array').that.is.not.empty;
-        response.body.data.roles[0].should.have.property('name', 'admin');
-        done();
-      });
-  });
-  
-  it('should return status 200 indicating 1 row deleted on /users/:userId DELETE', function (done) {
-    chai
-      .request(global.server)
-      .delete(`/users/${self.dummyUser.id}?force=true`)
-      .set('accessToken', global.authToken)
-      .end(function (error, response, body) {
-        response.should.have.status(200);
-        response.should.be.json;
-        response.body.should.have.property('status', 'success');
-        response.body.should.have.property('data').that.is.an('object');
-        response.body.data.should.have.property('rowsAffected', 1);
-        done();
-      });
+  describe('delete', function () {
+    it('should return status 401 unauthorized due to lack of auth token on /users DELETE', function (done) {
+      chai
+        .request(global.server)
+        .delete(`/users/${self.dummyUser.id}?force=true`)
+        .end(function (error, response, body) {
+          response.should.have.status(401);
+          response.should.be.json;
+          response.body.should.have.property('status', 'fail');
+          response.body.should.have.property('data').that.is.an('object');
+          response.body.data.should.have.property('message');
+          done();
+        });
+    });
+    
+    it('should return status 200 indicating 1 row deleted on /users/:userId DELETE', function (done) {
+      chai
+        .request(global.server)
+        .delete(`/users/${self.dummyUser.id}?force=true`)
+        .set('accessToken', global.authToken)
+        .end(function (error, response, body) {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.have.property('status', 'success');
+          response.body.should.have.property('data').that.is.an('object');
+          response.body.data.should.have.property('rowsAffected', 1);
+          done();
+        });
+    });
   });
 });
